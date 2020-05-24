bedd6f48b0b105340f29c88bd5db389a28cfe7f0
Denny <denny1011133@gmail.com>
2020-05-24 11:15:18 +0800

    Middleware test

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const totalTime = function (req, res, next) {
+  const start = new Date();
+  const date = start.getFullYear() + '-' + (start.getMonth() + 1) + '-' + start.getDate();
+  const time = start.getHours() + ":" + start.getMinutes() + ":" + start.getSeconds();
+  const requestTime = date + ' ' + time;
+  console.log(`requestTime:${requestTime}`);
+  res.on("finish", () => {
+    const end = new Date();
+    const totalPass = end - start
+    console.log(totalPass + 'ms')
+  })
+  next();
+}
+
+
+app.use(totalTime)
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
